e6094e7ba5eb4731a43c4ba2754092d9a11d2a89
lrb <lrbmike@163.com>
2023-10-29 17:29:09 +0800

    加入对话式问答链接口方法

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Request
 from ..util import pdf, langchain, fass
 
 router = APIRouter(
@@ -15,7 +15,7 @@ async def init_pdf():
     raw_text = pdf.get_pdf_text(pdf_doc)
 
     # get the text chunks
-    text_chunks = openai.get_text_chunks(raw_text)
+    text_chunks = langchain.get_text_chunks(raw_text)
 
     # save
     fass.save_vector_store(text_chunks)
@@ -32,5 +32,38 @@ async def question(
     chain = langchain.get_qa_chain(vector_store)
 
     response = chain({"query": text})
+    # reply = "回复:"
+    return {'success': True, "code": 0, "reply": response}
+
+
+@router.post("/question_history")
+async def question_history(
+        request: Request,
+        text: str = Body(embed=True)
+):
+    vector_store = fass.load_vector_store()
+
+    chain = langchain.get_history_chain(vector_store)
+
+    # 使用session缓存对话
+    chat_history = request["session"].get("chat_history")
+    if chat_history is None:
+        chat_history = []
+
+    # Convert chat history to list of tuples
+    chat_history_tuples = []
+    for message in chat_history:
+        chat_history_tuples.append((message[0], message[1]))
+
+    response = chain({"question": text, "chat_history": chat_history_tuples})
+
+    # 保存聊天记录
+    chat_history.append((text, response["answer"]))
+    # 处理chat_history的长度，避免token过长
+    chat_history = chat_history[0: 10]
+
+    # 重新保存到session
+    request["session"]["chat_history"] = chat_history
+
     # reply = "回复:"
     return {'success': True, "code": 0, "reply": response}
--- app/util/langchain.py
+++ app/util/langchain.py
@@ -1,4 +1,4 @@
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -29,4 +29,23 @@ def get_qa_chain(vector_store):
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
 
-    return RetrievalQA.from_llm(llm=ChatOpenAI(model_name='gpt-3.5-turbo-16k'), retriever=vector_store.as_retriever(), prompt=prompt)
+    return RetrievalQA.from_llm(llm=ChatOpenAI(model_name='gpt-3.5-turbo-16k'), retriever=vector_store.as_retriever(),
+                                prompt=prompt)
+
+
+# 获取对话式问答链
+def get_history_chain(vector_store):
+    prompt_template = """基于以下已知内容，简洁和专业的来回答用户的问题。
+                                            如果无法从中得到答案，清说"根据已知内容无法回答该问题"
+                                            答案请使用中文。
+                                            已知内容:
+                                            {context}
+                                            问题:
+                                            {question}"""
+
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+
+    return ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model_name='gpt-3.5-turbo-16k'),
+                                                 retriever=vector_store.as_retriever(),
+                                                 combine_docs_chain_kwargs={'prompt': prompt})
